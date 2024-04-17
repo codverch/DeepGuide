ab74fd843aa8b0964a40bb5a99102d478cea5220
Deepanjali Mishra <mishradeepanjali25@gmail.com>
2024-04-17 10:45:27 -0400

    setup

--- client-server-redis/redis_setup.sh
+++ client-server-redis/redis_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sudo apt-get update
+
 # Install Redis Dependencies
 sudo apt-get install libjemalloc-dev -y
 sudo apt-get install libgtest-dev -y
+sudo apt install redis-tools
 
 # Install OpenSSL Library to enable TLS support (not sure what TLS is)
 sudo apt-get install libssl-dev -y
@@ -26,3 +29,7 @@ make BUILD_TLS=yes
 # Test Redis
 # ./utils/gen-test-certs.sh
 # ./runtest --tls
+
+# To start the server: ./redis-server --port 9999 --protected-mode no
+# In the client: redis-cli -h <IP Address> -p <Port Number>
+# For example: redis-cli -h 128.105.145.101 -p 9999
